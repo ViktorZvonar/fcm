45e59ede366a902be388d0f1e1107883c47cb9e6
ViktorZvonar <108178380+ViktorZvonar@users.noreply.github.com>
2023-03-12 18:52:14 +0000

    Deploying to gh-pages from @ ViktorZvonar/goit-react-hw-08-phonebook@7db7cfcd1aa0971eb8b90f9ecef962b8426d5533 🚀

---
+++ static/js/520.3fd8eb13.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{4520:function(e,t,r){r.r(t);var n=r(1087),o=r(184);t.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h2",{children:"Page not found"}),(0,o.jsx)(n.rU,{to:"/",children:"To Home page"})]})}}}]);
+//# sourceMappingURL=520.3fd8eb13.chunk.js.map
