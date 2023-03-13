070662a6e5882d35ad156c8615eb47cdd9a0604c
ViktorZvonar <viktorzvonar@yahoo.com>
2023-03-13 13:37:13 +0200

    Update NavbarMenu.styled.js

--- src/components/Navbar/NavbarMenu/NavbarMenu.styled.js
+++ src/components/Navbar/NavbarMenu/NavbarMenu.styled.js
@@ -2,7 +2,10 @@ import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
 export const Link = styled(NavLink)`
-  padding: 5px;
+  padding: 5px 30px 5px 30px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 3px 3px grey;
+  background-color: #1976d2;
   color: white;
   text-decoration: none;
   font-weight: bold;
